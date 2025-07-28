a9bcc41593cbc49bf24416acdbaac10e1950345a
reelai <reelai@163.com>
2025-07-28 21:10:20 +0800

    update

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+defineProps<{
+  msg: string
+  isButtonDisabled: boolean
+}>()
+</script>
+
+<template>
+  <!-- 这个一个整体div, 里面的block元素自然是竖着排-->
+  <div class="greetings">
+    <h1 class="green">{{ msg + "<h1>" }}</h1>
+    <h3>
+      You have successfully created a project with
+      <a href="https://vite.dev/" target="_blank" rel="noopener">Vite</a> +
+      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
+       <hr/>
+      <button :disabled="isButtonDisabled">Button</button>
+      <span>myspan</span>
+      <p>{{ msg.length}} {{ msg }}</p>
+    </h3>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
---
+++ src/views/ReelaiView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="reelai">
+    <h1>This is an reelai page</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .reelai {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+    border-left: 1px solid var(--color-border);
+  }
+  .reelai h1 {
+    padding: 10px;
+  }
+}
+</style>
